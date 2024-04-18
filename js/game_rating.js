8413cf1f6ad56cf698ab5afd5d44b27cc55dd0af
Adrián Ivančík <adrian.ivancik@gmail.com>
2024-04-18 16:27:10 +0200

    1st game and therefore the template is complete, made my stuff available from the nav

---
+++ js/game_rating.js
@@ -0,0 +1,39 @@
+window.onload = function() {
+    // Get the form element
+    let form = document.getElementById('rating_form');
+
+    // Add submit event listener to the form
+    form.onsubmit = function(event) {
+        // Get form elements
+        let message = document.getElementById('message');
+        let pos = document.getElementById('pos');
+        let neg = document.getElementById('neg');
+        let lang = document.getElementById('lang');
+
+       // Check if the textarea is empty
+       if (message.value.trim() === '') {
+            message.classList.add('error');
+            event.preventDefault(); // Prevent form submission
+        } else {
+            message.classList.remove('error');
+        }
+
+        // Check if a radiobutton is selected
+        if (!pos.checked && !neg.checked) {
+            document.getElementById('for_pos').classList.add('error');
+            document.getElementById('for_neg').classList.add('error');
+            event.preventDefault(); // Prevent form submission
+        } else {
+            document.getElementById('pos').classList.remove('error');
+            document.getElementById('neg').classList.remove('error');
+        }
+
+        // Check if '----' is selected as the language
+        if (lang.value === 'none') {
+            lang.classList.add('error');
+            event.preventDefault(); // Prevent form submission
+        } else {
+            lang.classList.remove('error');
+        }
+    };
+};
