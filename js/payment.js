e8f3596ec03aec7fdffe258536a315aed6af4d86
martinpolerecky <polereckymartin@gmail.com>
2024-04-21 16:37:53 +0200

    fix: js now correctly displays prices+game titles for all games

--- js/payment.js
+++ js/payment.js
@@ -5,55 +5,74 @@ function getQueryParam(param) {
     return urlParams.get(param);
 }
 
-// Function to set image based on query parameter
+// Function to set image, price, title (and color if needed) based on query parameter
 function setImage() {
     const imageParam = getQueryParam('image');
-   //TODO zmenit cenu pripadne farbu pri payment
     switch(imageParam) {
         case 'nfs':
             document.getElementById('game').src = '../images/nfs.jpg';
             document.querySelector('style').textContent = document.querySelector('style').textContent.replace("background-image: url('../images/gta.jpg');", "background-image: url('../images/nfs2.jpg');");
+            document.getElementById('price').innerText = '4.99€';
             document.getElementById('title').innerText = 'Need for Speed';
-            /*
-            document.getElementById('left').src = '../images/nfs2';
-            document.getElementById('right').src = '../images/nfs2.jpg';
-            */
+            document.getElementById('title').style.color = 'white';
+            document.getElementById('price').style.color = 'white';
             break;
         case 'amogus':
             document.getElementById('game').src = '../images/sus.jpg';
             document.querySelector('style').textContent = document.querySelector('style').textContent.replace("background-image: url('../images/gta.jpg');", "background-image: url('../images/sussy_bussy.gif');");
+            document.getElementById('price').innerText = '4.99€';
             document.getElementById('title').innerText = 'Amogus';
             break;
         case 'pokemon':
             document.getElementById('game').src = '../images/pikachu.png';
             document.querySelector('style').textContent = document.querySelector('style').textContent.replace("background-image: url('../images/gta.jpg');", "background-image: url('../images/pokemon_bg.jpg');");
+            document.getElementById('price').innerText = '4.99€';
             document.getElementById('title').innerText = 'Pokemon: Stop';
+            document.getElementById('title').style.color = 'white';
+            document.getElementById('price').style.color = 'white';
             break;
         case 'gta':
             document.getElementById('game').src = '../images/gta.png';
             document.querySelector('style').textContent = document.querySelector('style').textContent.replace("background-image: url('../images/gta.jpg');", "background-image: url('../images/gtaa.png');");
+            document.getElementById('price').innerText = '29.99€';
             document.getElementById('title').innerText = 'GTA VI';
             break;
         case 'enshrouded':
             document.getElementById('game').src = '../images/enshrouded.jpg';
             document.querySelector('style').textContent = document.querySelector('style').textContent.replace("background-image: url('../images/gta.jpg');", "background-image: url('../images/enshrouded_wp.jpeg');");
+            document.getElementById('price').innerText = '29.99€';
             document.getElementById('title').innerText = 'Enshrouded';
             break;
         case 'bg3':
             document.getElementById('game').src = '../images/bg3.jpg';
             document.querySelector('style').textContent = document.querySelector('style').textContent.replace("background-image: url('../images/gta.jpg');", "background-image: url('../images/bgwp.jpg');");
+            document.getElementById('price').innerText = '49.99€';
             document.getElementById('title').innerText = 'Baldur\'s Gate 3';
+            document.getElementById('title').style.color = 'white';
+            document.getElementById('price').style.color = 'white';
             break;    
         case 'ror2':
             document.getElementById('game').src = '../images/ror2tn.jpg';
-            document.querySelector('style').textContent = document.querySelector('style').textContent.replace("background-image: url('../images/gta.jpg');", "background-image: url('../images/ror2g.jpg');");
+            document.querySelector('style').textContent = document.querySelector('style').textContent.replace("background-image: url('../images/gta.jpg');", "background-image: url('../images/ror2gp.jpg');");
+            document.getElementById('price').innerText = '39.99€';
             document.getElementById('title').innerText = 'Risk of Rain 2';
+            document.getElementById('title').style.color = 'white';
+            document.getElementById('price').style.color = 'white';
             break;   
-        case '../images/enshrouded.jpg':
-            document.getElementById('game').src = '../images/enshrouded.jpg';
-            document.querySelector('style').textContent = document.querySelector('style').textContent.replace("background-image: url('../images/gta.jpg');", "background-image: url('../images/enshrouded_wp.jpeg');");
-            document.getElementById('title').innerText = 'Enshrouded';
-            break;   
+        case 'cs2':
+            document.getElementById('game').src = '../images/csgob.png';
+            document.querySelector('style').textContent = document.querySelector('style').textContent.replace("background-image: url('../images/gta.jpg');", "background-image: url('../images/csgoa.png');");
+            document.getElementById('price').innerText = '4.99€';
+            document.getElementById('title').innerText = 'CS 2';
+            break;  
+        case 'creed':
+            document.getElementById('game').src = '../images/creeda.png';
+            document.querySelector('style').textContent = document.querySelector('style').textContent.replace("background-image: url('../images/gta.jpg');", "background-image: url('../images/creedb.png');");
+            document.getElementById('price').innerText = '18.99€';
+            document.getElementById('title').innerText = 'Assassins Creed: Vajnory';
+            document.getElementById('title').style.color = 'white';
+            document.getElementById('price').style.color = 'white';
+            break; 
     }
 }
 
@@ -63,7 +82,7 @@ window.onload = setImage;
 // Function to redirect to pay.html with image parameter
 function redirectToPay(image) {
     // when being redirected from the homepage
-    if (image === 'gta') {
+    if ((image === 'gta') || (image =='creed') || (image=='cs2')) {
         window.location.href = `html/pay.html?image=${image}`;
     }
     // when being redirected from the game's respective page
